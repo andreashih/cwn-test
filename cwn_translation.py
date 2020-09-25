75044d6c7e4a9b8e2c4e4fe5cadb03ebfe85d75d
andreashih <r08142004@ntu.edu.tw>
2020-09-25 15:34:50 +0800

    Create cwn_translation.py

---
+++ cwn_translation.py
@@ -0,0 +1,21 @@
+# %%
+from googletrans import Translator
+from nltk.corpus import wordnet as wn
+def search_word(word):
+    translator = Translator()
+    tran_result = translator.translate(word).text
+    # translate CH to EN
+    synset = wn.synsets(tran_result)
+    num = []
+    df = []
+    result = []
+    
+    for i in range(0, len(wn.synsets(tran_result))):
+        df.append(synset[i].definition()) # find the definition of the word
+        num.append('{}-{}'.format(str(wn.synset(synset[i].name()).offset()).zfill(8), wn.synset(synset[i].name()).pos())) 
+        # Find the id num of the synset. It has to be 8-digit with a pos tag.
+    
+    return { id_: def_ for id_, def_ in zip(num, df) }
+# %%
+search_word('朋友')
+# %%

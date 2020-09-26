ef7dd0cc69b4f52a9ca90da5165f2f7caa803567
Andrea Shih <r08142004@ntu.edu.tw>
2020-09-26 12:14:17 +0800

    new updates

---
+++ CwnGraph.py
@@ -0,0 +1,19 @@
+# %%
+from pprint import pprint
+from CwnGraph import CwnBase
+CwnBase.install_cwn("cwn_graph.pyobj")
+# %%
+from CwnGraph import CwnBase
+cwn = CwnBase()
+# %%
+lemmas = cwn.find_lemma("^朋友$")
+lemmas
+# %%
+senses = lemmas[0].senses
+senses
+# %%
+friend = senses[0]
+friend.relations
+# %%
+friend.synonym
+# %%

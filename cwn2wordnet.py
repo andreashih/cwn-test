e6f058ac982abdd7245dd6714c3cf526dd64c454
Andrea Shih <r08142004@ntu.edu.tw>
2020-09-30 15:37:45 +0800

    updates

--- cwn2wordnet.py
+++ cwn2wordnet.py
@@ -5,13 +5,13 @@ from googletrans import Translator
 from nltk.corpus import wordnet as wn
 
 def search_word(word):
-    cwn = CwnBase() # initialize cwn
+    cwn = CwnBase() # Initialize cwn
     cwn_senses = cwn.find_lemma(word)[0].senses
     
-    word1 = re.sub('\^|\$', '', word) # remove the anchors
+    word1 = re.sub('\^|\$', '', word) # Remove the anchors
 
-    translator = Translator() # initialize the translator
-    tran_result = translator.translate(word1).text # translate CH to EN
+    translator = Translator() # Initialize the translator
+    tran_result = translator.translate(word1).text # Translate CH to EN
     synset = wn.synsets(tran_result)
 
     num = []
@@ -19,12 +19,13 @@ def search_word(word):
     result = []
     
     for i in range(0, len(synset)):
-        df.append(synset[i].definition()) # find the definition of the word
+        df.append(synset[i].definition()) # Find the definition of the word
         num.append('{}-{}'.format(str(wn.synset(synset[i].name()).offset()).zfill(8), wn.synset(synset[i].name()).pos())) 
         # Find the id num of the synset. It has to be 8-digit with a pos tag.
     
     return cwn_senses, { id_: def_ for id_, def_ in zip(num, df) }
 
 # %%
-search_word("^朋友$")
+word = str(input("Please enter the word:"))
+search_word(word)
 # %%

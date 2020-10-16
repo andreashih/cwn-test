fb0bc8e351fcd52c68b947c21dc29ebf9f3ea2b5
andreashih <r08142004@ntu.edu.tw>
2020-10-16 15:17:42 +0800

    updates

--- asbc8k_cwn.py
+++ asbc8k_cwn.py
@@ -11,12 +11,15 @@ sorted_wordfreq = sorted(((k, v) for k, v in word_freq.items() if cjk.match(k)),
 # %% 取詞頻最高的 8000 個詞彙
 sorted_wordfreq[:8000]
 # %%
+# Extract words only
 def Extract(lst): 
     return [item[0] for item in lst] 
 #%%
+# Initialize cwn
 from CwnGraph import CwnBase
 cwn = CwnBase()
 # %%
+# Find asbc8k not in cwn
 words = Extract(sorted_wordfreq[:8000])
 asbc_in_cwn = []
 for word in words:
@@ -24,7 +27,25 @@ for word in words:
         asbc_in_cwn.append(word)
 
  # %%
+# Write .csv
 import pandas as pd
 df = pd.DataFrame(asbc_in_cwn, columns=["colummn"])
 df.to_csv('asbc8k_not_in_cwn.csv', index=False)
+# %%
+# Import huayu8000
+import pandas as pd
+df = pd.read_csv("huayu8000_NV.csv")
+# %%
+# Extracts words only
+words2 = df.詞彙.to_list()
+# %%
+# Find huayu8000 not in cwn
+huayu_in_cwn = []
+for word in words2:
+    word = re.sub('[/].+|[(].+[)]', '', word)
+    if len(cwn.find_lemma(word)) == 0:
+        huayu_in_cwn.append(word)
+# %%
+df3 = pd.DataFrame(huayu_in_cwn, columns=["colummn"])
+df3.to_csv('huayu8k_not_in_cwn.csv', index=False)
 # %%
